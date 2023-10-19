29b3dc6c111018ef4f1994ea325678779f3ea62f
Daniil Ugreninov <danik888844@gmail.com>
2023-10-19 19:37:47 +0600

    add Content

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
       <h1 style="flex: 1; text-align: center;">Просмотр данных с cookie</h1>
       <button style="margin-right: 20px;" @click="toggleModal">Добавить фабрику</button>
   </div>
+  <Content />
   <div v-if="openModal">
     <Modal @close="toggleModal" />
   </div>
@@ -10,10 +11,11 @@
 
 <script>
 import Modal from './components/Modal.vue'
+import Content from './components/Content.vue'
 
 export default {
   name: 'App',
-  components: {Modal},
+  components: {Modal, Content},
   data(){
     return{
       openModal: false
@@ -28,60 +30,19 @@ export default {
 </script>
 
 <style>
-body{
-  background-color: #ececec;
-}
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
-    background-color: transparent; 
+    background-color: white; 
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
     margin-top: 40px;
+    border-radius: 5px 5px 0 0;
   }
   .top-container{
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-bottom: 2px solid whitesmoke;
   }
-  button {
-    width: auto;
-    height: 30px;
-    appearance: none;
-    background-color: transparent;
-    border: 2px solid #1A1A1A;
-    border-radius: 5px;
-    box-sizing: border-box;
-    color: #3B3B3B;
-    cursor: pointer;
-    display: inline-block;
-    font-size: 16px;
-    font-weight: 600;
-    line-height: normal;
-    margin: 0;
-    outline: none;
-    text-align: center;
-    text-decoration: none;
-    transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
-    user-select: none;
-    -webkit-user-select: none;
-    touch-action: manipulation;
-    will-change: transform;
-  }
-
-button:disabled {
-  pointer-events: none;
-}
-
-button:hover {
-  color: #fff;
-  background-color: #1A1A1A;
-  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
-  transform: translateY(-2px);
-}
-
-button:active {
-  box-shadow: none;
-  transform: translateY(0);
-}
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h3>HEllo</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .container{
+        height: 100%;
+    }
+</style>
