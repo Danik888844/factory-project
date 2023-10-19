32d6bd2043203cf77b7659c7af79708504f57c5e
Daniil Ugreninov <danik888844@gmail.com>
2023-10-19 12:26:18 +0600

    feat add Modal file

---
+++ src/components/Modal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+        <h2>Добавить фабрику в cookie</h2>
+        <div>
+            <span>Город: </span>
+            <input type="text" />
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [],
+    methods: {
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+    .modal {
+        width: 600px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 5px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
